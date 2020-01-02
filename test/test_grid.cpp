a7290884f2838946e27fc161d97fd58cedc6f63f
Todd Flanders <todd.a.flanders@accenture.com>
2020-01-02 16:33:17 -0500

    rescue grid test

--- test/test_grid.cpp
+++ test/test_grid.cpp
@@ -1,39 +1,44 @@
 #include <stdbool.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <gtest/gtest.h>
 #include <string.h>
 #include <time.h>
-#include <render.h>
-#include "gameOfLife.h"
-#include "rules.h"
-#include "render.h"
-#include "neighbor_counter.h"
+
+extern "C" {
+    #include "render.h"
+    #include "gameOfLife.h"
+    #include "rules.h"
+    #include "render.h"
+    #include "neighbor_counter.h"
+}
 #include "support.hpp"
 
-const int numColsForGridTests = 4;
-const int numRowsForGridTests = 3;
-char gridData[numRowsForGridTests][numColsForGridTests];
-struct Grid grid = (struct Grid){(char*)gridData, numRowsForGridTests, numColsForGridTests};
+class GridTest : public ::testing::Test {
 
-TEST_GROUP(Grid);
+    protected:
 
-TEST_SETUP(Grid) {
-    wipeGrid(grid);
-}
+        const static int numColsForGridTests = 4;
+        const static int numRowsForGridTests = 3;
+        char gridData[numRowsForGridTests][numColsForGridTests];
+        struct Grid grid;
 
-TEST_TEAR_DOWN(Grid) {
-}
+        GridTest() {
+            grid = (struct Grid){(char*)gridData, numRowsForGridTests, numColsForGridTests};
+            wipeGrid(grid);
+        }
+};
 
-TEST(Grid, create_empty_grid) {
+TEST_F(GridTest, create_empty_grid) {
 
     for(int r = 0; r < grid.numRows; ++r) {
         for(int c = 0; c < grid.numCols; ++c) {
-           TEST_ASSERT_EQUAL(' ', getCell(grid, (struct Point){.x=c, .y=r}));
+           ASSERT_EQ(' ', getCell(grid, (struct Point){.x=c, .y=r}));
        }
     }
 }
 
-TEST(Grid, randomize_grid) {
+TEST_F(GridTest, randomize_grid) {
     randomizeGrid(grid, time(0));
     int numMarks = 0;
     for(int r = 0; r < grid.numRows; ++r) {
@@ -44,37 +49,37 @@ TEST(Grid, randomize_grid) {
        }
     }
     int numCells = grid.numCols * grid.numRows;
-    TEST_ASSERT_TRUE(numMarks > 0 && numMarks < numCells);
+    ASSERT_TRUE(numMarks > 0 && numMarks < numCells);
 }
 
-TEST(Grid, set_grid_cell) {
+TEST_F(GridTest, set_grid_cell) {
     setLivingCell(grid, (struct Point){.x=1, .y=2});  // good
-    TEST_ASSERT_EQUAL('X', getCell(grid, (struct Point){.x=1, .y=2}));
+    ASSERT_EQ('X', getCell(grid, (struct Point){.x=1, .y=2}));
 }
 
-TEST(Grid, middle_cell_has_one_living_neighbor) {
+TEST_F(GridTest, middle_cell_has_one_living_neighbor) {
     setLivingCell(grid, (struct Point){.x=0, .y=0});
-    TEST_ASSERT_EQUAL(1, count_neighbors(grid, (struct Point){.x=1, .y=1}));
+    ASSERT_EQ(1, count_neighbors(grid, (struct Point){.x=1, .y=1}));
 }
 
-TEST(Grid, top_left_cell_has_one_living_neighbor) {
+TEST_F(GridTest, top_left_cell_has_one_living_neighbor) {
     setLivingCell(grid, (struct Point){.x=0, .y=1});
-    TEST_ASSERT_EQUAL(1, count_neighbors(grid, (struct Point){.x=0, .y=0}));
+    ASSERT_EQ(1, count_neighbors(grid, (struct Point){.x=0, .y=0}));
 }
 
-TEST(Grid, bottom_right_cell_has_one_living_neighbor) {
+TEST_F(GridTest, bottom_right_cell_has_one_living_neighbor) {
     setLivingCell(grid, (struct Point){.x=3, .y=1});
-    TEST_ASSERT_EQUAL(1, count_neighbors(grid, (struct Point){.x=3, .y=2}));
+    ASSERT_EQ(1, count_neighbors(grid, (struct Point){.x=3, .y=2}));
 }
 
-TEST(Grid, live_cell_with_fewer_than_two_neighbors_dies)
+TEST_F(GridTest, live_cell_with_fewer_than_two_neighbors_dies)
 {
     setLivingCell(grid, (struct Point){.x=0, .y=0});
     tick(grid);
-    TEST_ASSERT_FALSE(isAlive(grid, (struct Point){0, 0}));
+    ASSERT_FALSE(isAlive(grid, (struct Point){0, 0}));
 }
 
-TEST(Grid, live_cell_with_two_or_three_neighbors_survives)
+TEST_F(GridTest, live_cell_with_two_or_three_neighbors_survives)
 {
     setGrid(grid,
         "XX  ",
@@ -82,10 +87,10 @@ TEST(Grid, live_cell_with_two_or_three_neighbors_survives)
         "    "
     );
     tick(grid);
-    TEST_ASSERT_TRUE(isAlive(grid, (struct Point){0, 0}));
+    ASSERT_TRUE(isAlive(grid, (struct Point){0, 0}));
 }
 
-TEST(Grid, live_cell_with_more_than_three_neighbors_dies)
+TEST_F(GridTest, live_cell_with_more_than_three_neighbors_dies)
 {
     setGrid(grid,
         "XXX.",
@@ -99,10 +104,10 @@ TEST(Grid, live_cell_with_more_than_three_neighbors_dies)
         "X.X.",
         ".X.."
     );
-    TEST_ASSERT_FALSE(isAlive(grid, (struct Point){.x=1, .y=0}));
+    ASSERT_FALSE(isAlive(grid, (struct Point){.x=1, .y=0}));
 }
 
-TEST(Grid, dead_cell_with_three_neighbors_becomes_live)
+TEST_F(GridTest, dead_cell_with_three_neighbors_becomes_live)
 {
    setGrid(grid,
         "X   ",
@@ -110,11 +115,11 @@ TEST(Grid, dead_cell_with_three_neighbors_becomes_live)
         "    "
     );
     tick(grid);
-    TEST_ASSERT_TRUE(isAlive(grid, (struct Point){1, 0}));
+    ASSERT_TRUE(isAlive(grid, (struct Point){1, 0}));
 
 }
 
-TEST(Grid, dead_cell_with_two_neighbors_stays_dead)
+TEST_F(GridTest, dead_cell_with_two_neighbors_stays_dead)
 {
     setGrid(grid,
         "XX  ",
@@ -122,23 +127,5 @@ TEST(Grid, dead_cell_with_two_neighbors_stays_dead)
         "    "
     );
     tick(grid);
-    TEST_ASSERT_FALSE(isAlive(grid, (struct Point){1, 0}));
-}
-
-
-TEST_GROUP_RUNNER(Grid)
-{
-    RUN_TEST_CASE(Grid, create_empty_grid);
-    RUN_TEST_CASE(Grid, randomize_grid);
-    RUN_TEST_CASE(Grid, set_grid_cell);
-    RUN_TEST_CASE(Grid, middle_cell_has_one_living_neighbor);    
-    RUN_TEST_CASE(Grid, top_left_cell_has_one_living_neighbor);
-    RUN_TEST_CASE(Grid, bottom_right_cell_has_one_living_neighbor);
-
-    RUN_TEST_CASE(Grid, live_cell_with_fewer_than_two_neighbors_dies);
-    RUN_TEST_CASE(Grid, live_cell_with_two_or_three_neighbors_survives);
-    RUN_TEST_CASE(Grid, live_cell_with_more_than_three_neighbors_dies);
-    RUN_TEST_CASE(Grid, dead_cell_with_three_neighbors_becomes_live);
-    RUN_TEST_CASE(Grid, dead_cell_with_two_neighbors_stays_dead);
-
+    ASSERT_FALSE(isAlive(grid, (struct Point){1, 0}));
 }
